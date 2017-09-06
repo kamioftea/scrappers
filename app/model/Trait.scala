ab7d943bea8b36851995ed365e12c8c7ba32aedd
Jeff Horton <jeff@goblinoid.co.uk>
2017-09-06 22:38:42 +0100

    Syntax / format fix

--- app/model/Trait.scala
+++ app/model/Trait.scala
@@ -1,10 +1,23 @@
-case class Trait(
-    name: String
-    description: String
-    resources: Map[String, Int]
-    tags: List[String]
-)
+package model
 
-class MemberTrait extends Trait
+sealed trait Trait {
+  val name: String
+  val description: String
+  val resources: Map[String, Int]
+  val tags: List[String]
+}
 
-class GearTrait extends Trait 
+case class WarbandTrait(name: String, description: String) extends Trait {
+  override val resources: Map[String, Int] = Map()
+  override val tags: List[String] = List()
+}
+
+case class MemberTrait(name: String,
+                       description: String,
+                       resources: Map[String, Int] = Map(),
+                       tags: List[String] = List()) extends Trait
+
+case class GearTrait(name: String,
+                     description: String,
+                     resources: Map[String, Int] = Map(),
+                     tags: List[String] = List()) extends Trait
--- app/model/Warband.scala
+++ app/model/Warband.scala
@@ -1,31 +1,42 @@
+package model
+
 case class Warband(faction: Faction, members: List[Member], traits: Set[WarbandTrait])
 
-case class Member (memberType: MemberType, cbt: Int, cmd: Int, con: Int, traits: Set[MemberTrait], gear: Set[Gear], xp: Int)
+case class Member(memberType: MemberType, cbt: Int, cmd: Int, con: Int, traits: Set[MemberTrait], gear: Set[Gear], xp: Int)
 
-sealed trait MemberType(val startingTraits: Int) {
-    abstract def create(implicit traitLookup: Map[String, MemberTrait]): Member
+sealed trait MemberType {
+  val startingTraits: Int
+
+  def create(implicit traitLookup: Map[String, MemberTrait]): Member
 }
 
 object MemberType {
-    case object Commander extends MemberType { 
-        val startingTraits = 3
-        override def create(implicit traitLookup: Map[String, MemberTrait]) =
-            Member(Commander, 1, 1, 1, Set(traitLookup("Commander")), Set(), 0)
-    }
-    case object Veteran extends MemberType { 
-        val startingTraits = 2
-        
-        override def create(implicit traitLookup: Map[String, MemberTrait]) =
-            Member(Veteran, 1, 1, 1, Set(), Set(), 0) 
-    }
-    case object Trooper extends MemberType { 
-        val startingTraits = 1 
-        
-        override def create(implicit traitLookup: Map[String, MemberTrait]) =
-            Member(Trooper, 1, 1, 1, Set(), Set(), 0)
-    }
+
+  case object Commander extends MemberType {
+    val startingTraits = 3
+
+    override def create(implicit traitLookup: Map[String, MemberTrait]) =
+      Member(Commander, 1, 1, 1, Set(traitLookup("Commander")), Set(), 0)
+  }
+
+  case object Veteran extends MemberType {
+    val startingTraits = 2
+
+    override def create(implicit traitLookup: Map[String, MemberTrait]) =
+      Member(Veteran, 1, 1, 1, Set(), Set(), 0)
+  }
+
+  case object Trooper extends MemberType {
+    val startingTraits = 1
+
+    override def create(implicit traitLookup: Map[String, MemberTrait]) =
+      Member(Trooper, 1, 1, 1, Set(), Set(), 0)
+  }
+
 }
 
-case class Faction(name: String, description: String, rivalFactionName: String, aliases: Map[MemberType, String], trait: Trait)
+case class Faction(name: String, description: String, rivalFactionName: String, aliases: Map[MemberType, String], factionTrait: Trait)
+
+case class Gear(name: String, traits: Set[GearTrait])
 
 
